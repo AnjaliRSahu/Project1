8c8b2755972312706753ed09fdd863b5d9a42e8d
AnjaliRSahu <anjalisahu1996@gmail.com>
2022-08-11 09:55:01 +0530

    added pp1.sh

---
+++ pp3.sh
@@ -0,0 +1,14 @@
+#!biin/bash -x
+read -p "Enter three numbers :" a b c ;
+c1=$a+$b*$c ;
+# c2=$c + $a / $b ;
+# c3=$a % $b + $c;
+# c4=$a * $b + $c;
+
+# if [ $c1 -gt $c2 ]
+#then
+ #   echo " $c1 is maximum";
+#else
+ #   echo " $c2 is maximum";
+#fi
+#
